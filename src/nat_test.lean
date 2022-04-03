c48d212cd9499b8dd15fb11c5116bcd5110c204e
Logan Weber <weberlo@cs.washington.edu>
2022-04-03 16:41:30 -0400

    Impl seems to work

--- src/nat_test.lean
+++ src/nat_test.lean
@@ -22,8 +22,15 @@ instance : inhabited point :=
 -- NOTE: This is the *squared* ℓ₂ norm.  It should be fine for our purposes,
 -- because it's a monotonic isomorphism, but we should think some more to make
 -- sure.
-instance : has_norm point :=
-  ⟨λ p, p.1 * p.1 + p.2 * p.2⟩
+-- instance : has_norm point :=
+--   ⟨λ p, ⟩
+
+-- NOTE: We don't want to use the `has_norm` typeclass, because that asserts the
+-- result is a real.
+def point_norm (p : point) : ℚ :=
+  p.1 * p.1 + p.2 * p.2
+
+local notation `∥` e `∥` := point_norm e
 
 #eval ((1, 2) : point) + ((-3, -4) : point)
 
@@ -41,13 +48,16 @@ def point_hash : point → ℕ
 
 def grid_2D := hash_map grid_idx (λ _, list point)
 
+def get_bucket_idx (p : point) (C : ℕ) : (ℤ × ℤ) :=
+  (p.1 / C, p.2 / C)
+
 -- `C` is an upper bound on the closest pair distance.
 def grid_points (C : ℕ) (C_nonzero : C ≠ 0) : list point → grid_2D
 | [] := mk_hash_map point_hash
 | (x :: xs) :=
     let g := grid_points xs in
     -- TODO could just use `g.modify`
-    let bucket_idx := (x.1 % C, x.2 % C) in
+    let bucket_idx := get_bucket_idx x C in
     let l := match g.find bucket_idx with
       | none := []
       | some l' := l'
@@ -61,45 +71,28 @@ def test_points : list point :=
 #check (grid_points 3 (by simp) test_points).entries
 -- def grid_points : list (Σ (_ : point), list point)
 
-def min_dist_point_aux (xs : list (point × ℝ)) (acc : point × ℝ) : point × ℝ :=
-  match xs with
-  | [] := acc
-  | ((p, d) :: pds) :=
-      let (p_acc, d_acc) := acc in
-      let acc' :=
-        if d < d_acc
-        then (p, d)
-        else acc
-      in
-      have pds.sizeof < xs.sizeof, by sorry,
-      have acc'.sizeof < acc.sizeof, by sorry,
-      min_dist_point_aux pds acc'
-  end
+def min_dist_point_aux : list (point × ℚ) → option (point × ℚ)
+| [] := none
+| ((p, d) :: pds) :=
+    match min_dist_point_aux pds with
+    | none :=
+        some (p, d)
+    | some (p', d') :=
+        some (if d < d' then (p, d) else (p', d'))
+    end
 
-def min_dist_point (pt_dist_pairs : list (point × ℝ)) : option point :=
-  match pt_dist_pairs with
-  | [] := none
-  | ((p, d) :: pds) :=
-      (min_dist_point_aux pt_dist_pairs (p, d)).fst
-
--- variable (a : option (point × ℝ))
--- #check a >>= (λ o, some o.fst)
-  -- match o with
-  -- | (some (p, _)) := p
-  -- | none := none
-  -- end)
+def min_dist_point : list (point × ℚ) → option point
+| [] := none
+| pds := min_dist_point_aux pds >>= (λ (p : point × ℚ), some p.1)
 
 def aux
   (C : ℕ)
   (C_nonzero : C ≠ 0)
-  (g : grid_2D)
-  (closest : point × point)
-  (points : list point)
-  : point × point :=
-match points with
-| [] := closest
+  (g : grid_2D) : list point → option (point × point)
+| [] := none
 | (p :: ps) :=
-    let bucket_idx := (p.1 % C, p.2 % C) in
+    let rec_res := aux ps in
+    let bucket_idx := get_bucket_idx p C in
     let kernel : list (ℤ × ℤ) :=
       [
         (-1, 1), (0, 1), (1, 1),
@@ -109,40 +102,56 @@ match points with
     in
     -- `N` = neighbors
     let N := kernel.map (λ offs, g.find (bucket_idx + offs)) in
-    let point_dist_pair_lists : list (list (point × ℝ)) := N.map (λ mq,
+    let point_dist_pair_lists : list (list (point × ℚ)) := N.map (λ mq,
       match mq with
       | none := []
       | (some l) := l.map (λ q, (q, ∥ p - q ∥))
       end
     ) in
+    -- let point_dist_pairs := point_dist_pair_lists.join.filter (λ qd, qd.1 ≠ p) in
     let point_dist_pairs := point_dist_pair_lists.join in
-    match min_dist_point point_dist_pairs with
-    | some q := sorry
-    | none := sorry
-    end
+    -- let _ := (by trace (repr point_dist_pairs)) in
+    trace (repr p) $
+    trace (repr bucket_idx) $
+    trace (repr point_dist_pairs) $
+    let curr_res :=
+      match min_dist_point point_dist_pairs with
+      | some q := some (p, q)
+      | none := none
+      end
+    in
+    match (curr_res, rec_res) with
+    | (some _, none) := curr_res
+    | (none, some _) := rec_res
+    | (some (p', q'), some (p'', q'')) :=
+        if ∥ p' - q' ∥ < ∥ p'' - q'' ∥
+        then curr_res
+        else rec_res
+    | (none, none) := none
 end
 
 /-
 We can only get `none` if `C` wasn't actually a valid hint.
 -/
-noncomputable def find_closest_pair
+def find_closest_pair
   (C : ℕ)
   (C_nonzero : C ≠ 0)
   (points : list point)
   : option (point × point) :=
-  match points with
-  | [] := none
-  | (_ :: []) := none
-  | (p₁ :: p₂ :: _) :=
-      let g := grid_points C C_nonzero points in
-      let (p₁', p₂') := aux C C_nonzero g (p₁, p₂) points in
-      -- TODO we need to use some norm that yeets out to ℚ, rather than ℝ, so it
-      -- can recognize this as computable.
-      if ∥ p₁' - p₂' ∥ > C
+  let g := grid_points C C_nonzero points in
+  aux C C_nonzero g points >>= (
+    λ pq : point × point,
+      if ∥ pq.1 - pq.2 ∥ > C
       then none
-      else some (p₁', p₂')
-  end
+      else some pq
+  )
 
+
+
+-- def test_points : list point :=
+--   [(0, 0), (2, 2)]
+
+#eval find_closest_pair 2 (by simp) test_points
 
 -- def hash_map_test : hash_map (ℚ × ℚ) (λ _, list point) :=
 --   let res := mk_hash_map {point} {λ _, list point} point_hash 31 in
